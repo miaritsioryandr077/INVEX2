826d2bea71c84b44011b4eb5e382474c60135e51
Miaritsiory ANDRIAMBOLOLONA <miaritsioryandriambololona@gmail.com>
2025-07-02 00:38:44 +0200

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,22 +1,7 @@
 // backend/routes/authRoutes.js
 const express = require('express');
 const router = express.Router();
-// const db = require('../config/db'); // ta connexion MySQL
 const authController = require('../controllers/authController');
-
-// router.post('/login', (req, res) => {
-//   const { email, password } = req.body;
-
-//   const sql = 'SELECT * FROM users WHERE email = ? AND password = ?';
-//   db.query(sql, [email, password], (err, results) => {
-//     if (err) return res.status(500).json({ message: 'Erreur serveur' });
-//     if (results.length === 0) {
-//       return res.status(401).json({ message: 'Identifiants incorrects' });
-//     }
-
-//     res.json({ message: 'Login successful' }); // tu pourrais renvoyer un token ici aussi
-//   });
-// });
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
